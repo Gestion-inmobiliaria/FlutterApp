3066e4cb9d9e66a6df588619d2b1a3223e8ba515
alexinho11 <58306419+AlexTiclla@users.noreply.github.com>
2025-04-03 19:29:56 -0400

    Probando git desktop
    nada importante

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,7 @@ import 'package:inmobiliaria_app/main.dart';
 
 void main() {
   print("Hello GitHub Desktop");
+  print("Probando la opcion de fetch");
 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
